915f70792bafd4d404ad5ed09ad36aa1b36c1eaa
pwangstustmis <37906966+pwangstustmis@users.noreply.github.com>
2024-08-14 14:52:39 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  //設定捲動指標設備
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      scrollBehavior: MyCustomScrollBehavior(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Hello'),
+      ),
+      body: Row(children:[
+        Container(width:300,height:300,color:Colors.red),
+        Container(width:300,height:300,color:Colors.green),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: '按鈕說明文字',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
